0222f871dfeb3ba6cf93e414a7085f6356e80ff9
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-09-26 11:21:06 -0700

    add button and fix credit

--- script.js
+++ script.js
@@ -343,3 +343,24 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// Dropup menu
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("dropupcon").classList.toggle("show");
+}
+
+// Close the dropup menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropupbtn')) {
+    var dropdowns = document.getElementsByClassName("dropup-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
